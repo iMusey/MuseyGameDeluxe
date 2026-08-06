bd53dc33b41711e2b66c7afde868a3e48bfcffd3
iMusey <alexander.farahany@gmail.com>
2026-08-05 21:58:18 -0500

    it moves!!

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Vector2 moveVal;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate((new Vector3(moveVal.x, moveVal.y, 0)).normalized*Time.deltaTime);
+    }
+    public void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("fire");
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.control);
+        moveVal = context.ReadValue<Vector2>();
     }
 }
